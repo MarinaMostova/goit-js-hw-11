8be57e190b718f748993791ce24dac4ed376e88d
M.Mostova <mmainfo15@gmail.com>
2023-02-25 00:29:39 +0200

    Домашнє завдання 11

---
+++ src/index.js
@@ -0,0 +1,97 @@
+import ImageService from './js/image-servise';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+  buttonLoadMore: document.querySelector('.load-more'),
+};
+
+const imageService = new ImageService();
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.buttonLoadMore.addEventListener('click', onLoadMore);
+refs.buttonLoadMore.classList.add('is-hidden');
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  imageService.query = e.currentTarget.elements.searchQuery.value.trim();
+  imageService.resetPage();
+  imageService.fetchImages().then(handleResponse).catch(onFetchError);
+  dataCleaning();
+}
+
+function handleResponse(data) {
+  if (data.totalHits === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    dataCleaning();
+    return;
+  }
+  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  createGallery(data.hits);
+  lightbox();
+  refs.buttonLoadMore.classList.remove('is-hidden');
+}
+
+function onLoadMore(event) {
+  imageService
+    .fetchImages()
+    .then(data => {
+      createGallery(data.hits);
+      lightbox().refresh();
+    })
+    .catch(onFetchError);
+}
+
+function createGallery(images) {
+  console.log(images);
+
+  const markup = images
+    .map(
+      image =>
+        `<div class="photo-card">
+        <a  href="${image.largeImageURL}">   
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="280px" height="180px"/>
+          </a>
+       <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          ${image.likes} </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${image.views} </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${image.comments} </p>
+        <p class="info-item">
+          <b>Downloads</b> ${image.downloads}</p>
+             </div>
+    </div>`
+    )
+    .join('');
+
+  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+function lightbox() {
+  return new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+}
+
+function onFetchError(error) {
+  Notiflix.Notify.warning('An error has occurred. Please try again.');
+}
+
+function dataCleaning() {
+  refs.galleryContainer.innerHTML = '';
+  refs.buttonLoadMore.classList.add('is-hidden');
+}
+
---
+++ src/js/image-servise.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+export default class ImageService {
+  constructor() {
+    this.searchOuery = '';
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchOuery;
+  }
+
+  set query(newSearchQeury) {
+    this.searchOuery = newSearchQeury;
+  }
+
+  async fetchImages() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = '33857774-d4152e7dd23a1d1cd9f80d986';
+    try {
+      const response = await axios.get(
+        `${BASE_URL}?key=${API_KEY}&q=${this.searchOuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
+      );
+      this.page += 1;
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
